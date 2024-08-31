0384abe87fc438cbf6f2d441f46d119cc1cf8610
Abdullh1111 <abdullah4474032@gmail.com>
2024-08-31 07:35:16 +0600

    create product model

---
+++ backend/src/module/product/product.model.ts
@@ -0,0 +1,13 @@
+import { model, Schema } from "mongoose";
+import { TProduct } from "./product.interface";
+
+const productScheema = new Schema<TProduct>({
+    productName:{type:String, required:true},
+    price: {type: Number, required: true},
+    details: {type: String, required: true},
+    rating: {type: Number},
+    productImage: {type: String, required: true}
+})
+
+
+export const product = model<TProduct>('product',productScheema)
